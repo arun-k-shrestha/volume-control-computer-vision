ac78d040e9198b42a54926639c0a08399225f26f
Arun Shrestha <76599239+arun-k-shrestha@users.noreply.github.com>
2021-07-13 14:37:06 -0700

    Update Module.py

--- Module.py
+++ Module.py
@@ -1,7 +1,5 @@
 import cv2 
 import mediapipe as mp
-import time
-from mediapipe.python import solutions
 
 class handDetector():
     def __init__(self, 
@@ -27,7 +25,6 @@ class handDetector():
 
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
-        #print(results.multi_hand_landmarks)
 
         if self.results.multi_hand_landmarks:
             for handmrk in self.results.multi_hand_landmarks:
@@ -44,39 +41,8 @@ class handDetector():
             for id, lm in enumerate(Myhand.landmark):
                 height, width, channel = img.shape
                 center_x, centre_y = int(lm.x*width), int(lm.y*height)
-                #print(id, center_x, centre_y)
                 lis.append([id, center_x,centre_y])
                 if draw:
                     cv2.circle(img, (center_x, centre_y),15, (0,0,255), 10 )
 
         return lis
-
-
-def main():
-
-    PreviousTime = 0
-    CurrentTime = 0
-    VC = cv2.VideoCapture(0) # video object
-
-    detector= handDetector()
-
-
-    while True:
-        success, img = VC.read()
-        img = detector.FindHands(img, draw = False)
-        lis = detector.findPosition(img, draw = False)
-        if len(lis) != 0:
-            print(lis[4])
-
-        CurrentTime = time.time() 
-        fps = 1/(CurrentTime-PreviousTime)
-        PreviousTime = CurrentTime
-
-        cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,0,225), 2)
-
-        cv2.imshow("Image", img)
-
-        cv2.waitKey(1)
-    
-if __name__== "__main__":
-    main()
