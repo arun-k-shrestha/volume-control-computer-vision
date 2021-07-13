3cfea5830ae5a45b5e1ab4038ec81e13d829b4f5
Arun Shrestha <76599239+arun-k-shrestha@users.noreply.github.com>
2021-07-13 14:36:24 -0700

    Update VolumeControl.py

--- VolumeControl.py
+++ VolumeControl.py
@@ -5,27 +5,23 @@ import Module
 import math 
 import osascript
 
-# from ctypes import cast, POINTER
-# from comtypes import CLSCTX_ALL
-# from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
 
 WIDTH_CAMERA = 1280
 HEIGHT_CAMERA = 720
 
-cap = cv2.VideoCapture(0) #id 0
+cap = cv2.VideoCapture(0)
 cap.set(3, WIDTH_CAMERA)
 cap.set(4, HEIGHT_CAMERA)
 PreviousTime = 0
 
-
 detector = Module.handDetector(min_detection_confidence = 0.7)
 
 while True:
-    success, img = cap.read()
-    img = detector.FindHands(img)
+    any_variable, img = cap.read()
+    img = detector.FindHands(img,draw= False)
     lis = detector.findPosition(img,draw=False)
     if len(lis) != 0:
-        #print(lis[4], lis[8])
 
         Thumb_X, Thumb_Y = lis[4][1], lis[4][2]
         Index_X, Index_Y = lis[8][1], lis[8][2]
@@ -35,13 +31,11 @@ while True:
         cv2.line(img, (Thumb_X,Thumb_Y),(Index_X,Index_Y), (0,0,255), 3)
 
         Length = math.hypot(Index_X - Thumb_X, Index_Y -Thumb_Y)
-        #print(Length)
 
         Main_Volume = np.interp(Length, [50,300],[0, 100])
-        
         vol = "set volume output volume " + str(Main_Volume)
         osascript.osascript(vol)
-
+        
 
     CurrentTime = time.time()
     fps = 1/(CurrentTime- PreviousTime)
